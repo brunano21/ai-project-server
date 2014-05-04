b3ebd794e13dd5244e699b0786811859d9dc25de
giorgio-ciacchella <giorgio.ciacchella@gmail.com>
2014-05-04 16:27:02 -0400

    commit

--- src/main/java/ai/server/filter/FiltroUtente.java
+++ src/main/java/ai/server/filter/FiltroUtente.java
@@ -1,7 +1,11 @@
 package ai.server.filter;
 
+import hibernate.Profilo;
+import hibernate.Utente;
+
 import java.io.IOException;
 import java.security.Principal;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,6 +13,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
@@ -26,6 +31,7 @@ import dati.Dati;
 public class FiltroUtente implements Filter {
 
 	private ApplicationContext context;
+	private Dati dati;
     /**
      * Default constructor. 
      */
@@ -47,12 +53,25 @@ public class FiltroUtente implements Filter {
 		// TODO Auto-generated method stub
 		// place your code here
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		Dati dati = (Dati)context.getBean("dati");
-		
-		System.out.println(dati.getProdotti().size());
-		// pass the request along the filter chain
+		HttpServletResponse resp = (HttpServletResponse) response;
+		if(auth.getName() != "anonymousUser"){
+			if(dati != null){
+				Utente utente = dati.getUtenti().get(auth.getName());
+				Profilo profilo = null;
+				for(Profilo p : (Set<Profilo>) utente.getProfilos()){
+					profilo = p;
+					break;
+				}
+				
+				if(profilo.getReputazione() == 0){
+					request.setAttribute("dati", dati);
+					request.setAttribute("error", "Non hai i requisiti per andare in quella pagina");
+					request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
+				}
+			}
+		}
 		chain.doFilter(request, response);
+		// pass the request along the filter chain
 	}
 
 	/**
@@ -61,6 +80,7 @@ public class FiltroUtente implements Filter {
 	public void init(FilterConfig fConfig) throws ServletException {
 		// TODO Auto-generated method stub
 		context = WebApplicationContextUtils.getWebApplicationContext(fConfig.getServletContext());
+		dati = (Dati)context.getBean("dati");
 	}
 
 }
